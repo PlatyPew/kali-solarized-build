9c83f879da0c3c62fbf45a5fd1c88ef618ff7342
PlatyPew <daryl.limkz@gmail.com>
2018-12-01 21:29:53 +0800

    forgot we're not using a smart language

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+# Pull modules
+git submodule init
+git submodule update --remote --recursive
+
+# Copy relevant files
 cp rice.list.chroot live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
+
+# Live build
+cd live-build-config
+./build.sh --variant i3wm --verbose
