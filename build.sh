20b1b06f7aa089aca756b6f076b2f438dc98ba63
PlatyPew <daryl.limkz@gmail.com>
2018-12-02 11:35:52 +0800

    It's getting hard to keep up with the crap I've trashed

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # Ensure script is executed in correct directory
-cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd ${DIR}
 
 # Pull modules
 git submodule init
@@ -8,20 +9,29 @@ git submodule update --remote --recursive
 # Include packages
 cp rice-kali.list.chroot live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
 
+# Include hooks
+
+
 # Copy wallpapers
 mkdir -p ./live-build-config/kali-config/common/includes.chroot/root/Pictures
+mkdir -p ./live-build-config/kali-config/common/includes.binary/root/Pictures
 unzip wallpapers.zip -d ./live-build-config/kali-config/common/includes.chroot/root/Pictures
+unzip wallpapers.zip -d ./live-build-config/kali-config/common/includes.binary/root/Pictures
 
 # Copy fonts
 mkdir -p ./live-build-config/kali-config/common/includes.chroot/root/.local/share/fonts
+mkdir -p ./live-build-config/kali-config/common/includes.binary/root/.local/share/fonts
 cp 'Inconsolata Nerd Font Complete.otf' ./live-build-config/kali-config/common/includes.chroot/root/.local/share/fonts
+cp 'Inconsolata Nerd Font Complete.otf' ./live-build-config/kali-config/common/includes.binary/root/.local/share/fonts
 
 # Install configs
 git clone https://github.com/PlatyPew/dotfiles.git ./live-build-config/kali-config/common/includes.chroot/root/.config
+git clone https://github.com/PlatyPew/dotfiles.git ./live-build-config/kali-config/common/includes.binary/root/.config
 
 # Install vim plug
-curl -fLo ./live-build-config/kali-config/common/includes.chroot/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ./live-build-config/kali-config/common/root/includes.chroot/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ./live-build-config/kali-config/common/root/includes.binary/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Begin live build
 cd live-build-config
-./build.sh --variant i3wm --verbose
+#./build.sh --variant i3wm --verbosbinary
