b552c2b43f923c21d6160081209c14534936b58c
PlatyPew <daryl.limkz@gmail.com>
2018-12-02 09:19:10 +0800

    oops!

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
+# Ensure script is executed in correct directory
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
+
 # Pull modules
 git submodule init
 git submodule update --remote --recursive
 
-# Copy relevant files
+# Include packages
 cp rice-kali.list.chroot live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
 
-# Live build
+# Copy wallpapers
+mkdir -p ./live-build-config/kali-config/common/includes.chroot/root/Pictures
+unzip wallpapers.zip -d ./live-build-config/kali-config/common/includes.chroot/root/Pictures
+
+# Copy fonts
+mkdir -p ./live-build-config/kali-config/common/includes.chroot/root/.local/share/fonts
+cp 'Inconsolata Nerd Font Complete.otf' ./live-build-config/kali-config/common/includes.chroot/root/.local/share/fonts
+
+# Begin live build
 cd live-build-config
 ./build.sh --variant i3wm --verbose
