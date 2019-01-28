783e8bf5f1e11edcec61af52aecb21c8062a2e9e
PlatyPew <daryl.limkz@gmail.com>
2019-01-28 12:46:09 +0800

    Added installation scripts

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ git submodule update --remote --recursive
 # Include packages
 cp rice-kali.list.chroot ./live-build-config/kali-config/variant-i3wm/package-lists/kali.list.chroot
 
+# Include install scripts
+cp install-scripts/* live-build-config/kali-config/common/hooks/live
+
 # Copy wallpapers
 mkdir -p live-build-config/kali-config/common/includes.chroot/root/Pictures
 cp -r solarized_wallpaper ./live-build-config/kali-config/common/includes.chroot/root/Pictures
